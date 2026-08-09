d5654f0d7ecb117d4a264842793b65fc912ea34d
ronitverma28 <ronitroy22678@gmail.com>
2026-08-09 14:18:13 +0530

    backend endpoint added

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://ecommerce-application-1561.onrender.com/api/v1';
